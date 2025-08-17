f523955b9edf9e9220ddf78db77a67dae5e395bc
ivan <vanamelcikov7275@gmail.com>
2025-08-17 17:40:21 +0400

    lint: handlint

--- src/inboxium/main.py
+++ src/inboxium/main.py
@@ -1,4 +1,3 @@
-
 import argparse
 from email.parser import Parser
 
@@ -13,11 +12,13 @@ class InboxServer(smtpd.SMTPServer):
     """Logging-enabled SMTPServer instance with handler support."""
 
     def __init__(self, handler, *args, **kwargs):
+        """Init the inbox server class."""
         super(InboxServer, self).__init__(*args, **kwargs)
         self._handler = handler
 
     def process_message(self, peer, mailfrom, rcpttos, data):
-        log.info(f"Collating message from {mailfrom}")
+        """Work with message."""
+        log.info("Collating message from {}", mailfrom)
         subject = Parser().parsestr(data)["subject"]
         log.debug(dict(to=rcpttos, sender=mailfrom, subject=subject, body=data))
         return self._handler(to=rcpttos, sender=mailfrom, subject=subject, body=data)
@@ -26,31 +27,39 @@ class InboxServer(smtpd.SMTPServer):
 class Inbox:
     """A simple SMTP Inbox."""
 
-    def __init__(self, port=None, address=None):
+    def __init__(
+        self, port: str | int | None = None, address: str | None = None,
+    ) -> None:
+        """Init the inbox class."""
         self.port = port
         self.address = address
         self.collator = None
 
     def collate(self, collator):
-        """Function decorator. Used to specify inbox handler."""
+        """Func decorator. Used to specify inbox handler."""
         self.collator = collator
+        print(type(collator))
         return collator
 
-    def serve(self, port=None, address=None):
-        """Serves the SMTP server on the given port and address."""
+    def serve(
+        self,
+        port: str | int | None = None,
+        address: str | None = None,
+    ) -> None:
+        """Serve the SMTP server on the given port and address."""
         port = port or self.port
         address = address or self.address
 
-        log.info(f"Starting SMTP server at {address}:{port}")
+        log.info("Starting SMTP server at {}:{}", address, port)
 
-        server = InboxServer(self.collator, (address, port), None)
+        InboxServer(self.collator, (address, port), None)
 
         try:
             asyncore.loop()
         except KeyboardInterrupt:
             log.info("Cleaning up")
 
-    def dispatch(self):
+    def dispatch(self) -> None:
         """Command-line dispatch."""
         parser = argparse.ArgumentParser(description="Run an Inbox server.")
 
